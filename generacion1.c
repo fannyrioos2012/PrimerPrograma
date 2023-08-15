a2fe153aa1af3c714bf6c34f4ca0ceb37a42d258
fannyrioos2012 <142334991+fannyrioos2012@users.noreply.github.com>
2023-08-15 10:13:00 -0600

    Add files via upload

---
+++ generacion1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+int main (int argc, char **argv)
+{
+	system("color f9");
+setlocale(LC_ALL, "");
+	
+printf("GENERACIONES DE LAS COMPUTADORAS Y SUS CARACTERISTICAS\n"
+"\n"
+"\n"
+"Primera generación (decada de 1940-1950)\n"
+"Utilizaban tubos de vacío para procesar información. Eran grandes, costosas y generaban mucho calor\n"
+"\n"
+"Características\n"
+"-Utilizaban válvulas y tarjetas perforadas\n"
+"-Eran grandes, costosas\n"
+"-Se programaban en lenguaje de máquina\n"
+"Segunda generación decada (decada 1950-1969)\n"
+"Introducción de transistores, lo que redujo el tamaño y el consumo de energía de las computadoras. Aparecieron las cintas magnéticas y las tarjetas perforadas\n"
+"\n"
+"Características\n"
+"-Empleaban transistores en lugar de válvulas, lo que las hacía más pequeñas y eficientes\n"
+"-Aparecieron las cintas magnéticas y los lenguajes de programación de alto nivel\n"
+"\n"
+"Tercera generación (década de 1960-1970)\n"
+"Uso de circuitos integrados que permitieron una mayor capacidad de procesamiento. Surgieron los lenguajes de programación de alto nivel\n"
+"\n"
+"Características\n"
+"\n"
+"-Se introdujeron los circuitos integrados, lo que permitió un mayor rendimiento y confiabilidad\n"
+"-Surgieron los minicomputadoras y los sistemas operativos\n"
+"-Se empezaron a usar lenguajes de programación como COBOL y FORTRAN\n"
+"\n"
+"Cuarta generación (década de 1970-1980)\n"
+"Microprocesadores, que llevaron a la creación de las computadoras personales. Se desarrollaron sistemas operativos más avanzados y se popularizó la interfaz gráfica\n"
+"\n"
+"Características\n"
+"-Llegaron los microprocesadores, que integraban miles de transistores en un solo chip\n"
+"-Se popularizaron las computadoras personales y se avanzó en la interconexión en redes\n"
+"-Aparecieron lenguajes de programación\n"
+"\n"
+"Quinta generación (década de 1980 en adelante)\n"
+"\n"
+"Enfoque en la inteligencia artificial y el procesamiento paralelo. Desarrollo de supercomputadoras y avances en redes de comunicación\n"
+"\n"
+"Características\n"
+"-Se enfocaron en la inteligencia artificial y el procesamiento\n"
+);
+return ;
+}
